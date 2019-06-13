10e8a26d7327710b568a8c17e61a61272240a5aa
shengcui <shengcui@tencent.com>
2019-06-13 17:24:46 +0800

    update UGCDemo/ViewController

--- UGCDemo/UGCDemo/ViewController.m
+++ UGCDemo/UGCDemo/ViewController.m
@@ -100,7 +100,7 @@
             };
             
             UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:previewController];
-            [nav presentViewController:nav animated:YES completion:nil];
+            [weakConfigVC.navigationController presentViewController:nav animated:YES completion:nil];
         };
         [weakConfigVC.navigationController pushViewController:recordVC animated:YES];
     };
